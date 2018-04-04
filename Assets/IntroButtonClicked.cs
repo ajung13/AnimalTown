35b37e45ed3b6c00a259a4c7cfdefc459d9e08fc
ajung <cwhtha@gmail.com>
2018-04-04 15:53:14 +0900

    0404 intro(2)

---
+++ Assets/IntroButtonClicked.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroButtonClicked : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void gameStart(){
+		SceneManager.LoadScene ("AnimalOnWindow");
+	}
+}
